eb322a931925e256103d91fe9336af150ad38f65
Michael Steinberg <msteinbergtx@gmail.com>
2012-09-22 15:50:43 -0400

    test mongoid

---
+++ test.rb
@@ -0,0 +1,15 @@
+class Artist
+  include Mongoid::Document
+  field :name, type: String
+  embeds_many :instruments
+end
+
+class Instrument
+  include Mongoid::Document
+  field :name, type: String
+  embedded_in :artist
+end
+
+syd = Artist.where(name: "Syd Vicious").between(age: 18..25).first
+syd.instruments.create(name: "Bass")
+syd.with(database: "bands", session: "backup").save!
