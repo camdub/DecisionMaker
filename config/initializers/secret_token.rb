e4e93425469f95cfa36c0280c7ef8b13bd9533ad
Cameron Woodmansee <cameron29w@gmail.com>
2012-10-04 12:51:13 -0400

    Created new rails project and updated readme. This rails project was generated with the -T command because I plan on adding rspec.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Decisionmakr::Application.config.secret_token = '5947b65565bb1c7bdc246f5a70bf1f349b26f87e8036583bb85cc5b959be8475b524e3b6700a14072abd9f478dd96a1ef4b4e77b049a3b1f5b88150105f65622'
