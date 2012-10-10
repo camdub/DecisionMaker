1dc5602795b6ae780c952f48626a1ad8f86d69b4
Cameron Woodmansee <cameron29w@gmail.com>
2012-10-10 11:42:56 -0400

    Basic Event form with validation. Adding daterangepicker.

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,39 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    puts @event
+
+    if @event.save
+      redirect_to @event, notice: 'Event created successfully.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  private # strong params
+
+  def event_params
+    params.require(:event).permit(:title, :start_date, :end_date, :location)
+  end
+
+end
+
+
