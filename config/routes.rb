31188aee3ea070aafd3f685e01984f4812df4e22
Cameron Woodmansee <cameron29w@gmail.com>
2012-10-10 11:42:32 -0400

    Basic Event form with validation. Adding daterangepicker.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Decisionmakr::Application.routes.draw do
+  resources :events
+
+  root to: 'events#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
