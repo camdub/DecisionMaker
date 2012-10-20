1482bb25c7f900a906f9ada720abefe4b6e05a21
Cameron Woodmansee <cameron29w@gmail.com>
2012-10-19 22:12:26 -0400

    changed out the twitter tokens for environment variables. rookie mistake.

--- config/initializers/tweetstream.rb
+++ config/initializers/tweetstream.rb
@@ -1,7 +1,7 @@
 TweetStream.configure do |config|
-  config.consumer_key       = 'sypjG2Y2TPAIEaZ8bLuHA'
-  config.consumer_secret    = '8sBtR3MhKkQDEqKAsSXvy5TK52PskXWFRIaHrEpB8'
-  config.oauth_token        = '840210782-5nKwdtB901afCHNMekv4kHZ3NRJIh1Xs8HdQyqxR'
-  config.oauth_token_secret = 'rQpjGg9Rjv8PPvvFjUfPiFNNjjWKExgmcQLK89B3q0'
+  config.consumer_key       = ENV['CONSUMER_KEY']
+  config.consumer_secret    = ENV['CONSUMER_SECRET']
+  config.oauth_token        = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
   config.auth_method        = :oauth
 end
