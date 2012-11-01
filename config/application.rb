3a8f3dc57da05224d2f547f7fcdb086cc5f73668
Cameron Woodmansee <cameron29w@gmail.com>
2012-10-31 23:12:35 -0400

    added additional manifest file

--- config/application.rb
+++ config/application.rb
@@ -59,11 +59,13 @@ module Decisionmakr
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
+    #
+
+    config.assets.precompile += %w( mobile.css, mobile.js )
+    config.assets.initialize_on_precompile = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
